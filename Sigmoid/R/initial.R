c2888af717fd57c28c1edddae0296cfb72d13523
Girish Gupta <girish@girish-gupta.com>
2020-03-31 15:53:07 +0530

    Switches to China

--- Sigmoid/R/initial.R
+++ Sigmoid/R/initial.R
@@ -1,11 +1,11 @@
 library(httr)
 library(jsonlite)
 
-url = "https://api.datadrum.com/json/cv.IN_cases_cum" #Get data from ECDC (via Data Drum)
+url = "https://api.datadrum.com/json/cv.CN_cases_cum" #Get data from ECDC (via Data Drum)
 httpResponse <- GET(url, add_headers("token"="covid19"), accept_json())
 raw_data <- fromJSON(content(httpResponse,"text"))
 
-y=raw_data$data$cv___IN_cases_cum #Get it into y
+y=raw_data$data$cv___CN_cases_cum #Get it into y
 
 i=0
 ToDitch <- vector() # Create an empty array of values to ditch
@@ -37,20 +37,3 @@ data <- data.frame(x,y)
 
 fit <- nls(data$y ~ SSlogis(data$x, Asym, xmid, scal), data = data)
 print(fit)
-
-
-
-# x = seq(1:1000)
-# y = 81109.475/(1+exp((22.313-x)/4.462))
-
-# norm <- function(x) {x/max(x)}
-# y_norm <- norm(y)
-# 
-# print (y_norm)
-
-# require(nplr)
-# 
-# sigmoid_fit <- nplr(x=x, y=y_norm)
-# plot(sigmoid_fit, main="China")
-# print(sigmoid_fit)
-
